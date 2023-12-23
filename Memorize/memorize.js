70b5fd9b2cb8a6b64c85e038b297fed7c9f5822c
RyanD177 <w0477770@campus.nscc.ca>
2023-12-23 02:51:13 -0400

    Callback,map,cache

---
+++ Memorize/memorize.js
@@ -0,0 +1,37 @@
+function memorize(callback, resolver) {
+
+    let cache = new Map();
+
+    function getCacheKey(...args) {
+        return resolver != null ? resolver(...args) : JSON.stringify(args);
+
+
+    }
+    const memoized = function(...args) {
+        const cacheKey = getCacheKey(args);
+        if(cache.has(cacheKey)){
+            return cache.get(cacheKey);
+        }
+
+        const output = callback(...args)
+        cache.set(cacheKey, output)
+        return output;
+
+    }
+
+    memoized.clear = function () {
+        cache.clear()
+    }
+    memoized.delete = function () {
+        const cacheKey = getCacheKey(...args);
+        cache.delete(cacheKey);
+
+    }
+    memoized.has = function () {
+
+        
+    }
+
+
+
+}
